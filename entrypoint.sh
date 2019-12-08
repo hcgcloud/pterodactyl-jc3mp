40d0912de1d2af88f572cb6d018637f04772468f
git <git@hcgcloud.com>
2019-12-08 22:34:46 +0800

    First commit

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sleep 2
+
+cd /home/container
+
+# Update Server
+if [ "${SERVER_AUTOUPDATE}" == "1" ]; then
+    ./steam/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update 619960 +quit
+fi
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+${MODIFIED_STARTUP}
